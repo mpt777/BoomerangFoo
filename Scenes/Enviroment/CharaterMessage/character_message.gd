c8ddb2560435b09400972d5454457ef9390f2fba
mpt777 <mptaylor777@gmail.com>
2024-03-21 19:40:59 -0600

    messages

--- Scenes/Enviroment/CharaterMessage/character_message.gd
+++ Scenes/Enviroment/CharaterMessage/character_message.gd
@@ -1,11 +1,40 @@
-extends Node
+extends Label3D
+class_name CharacterMessage
 
 
-# Called when the node enters the scene tree for the first time.
+var seconds : float = 5.0
+var color : Color
+@onready
+var n_sprite := $Sprite3D
+@onready
+var n_timer := $Timer
+
+
 func _ready():
-	pass # Replace with function body.
+	set_color(Color.REBECCA_PURPLE)
+	set_seconds(5.0)
+	set_text("Debug2")
+	
+func set_seconds(seconds : float) -> void:
+	n_timer.wait_time = seconds
 
+func set_color(color: Color) -> void:
+	var gradient_data := {
+		0.0: Color.RED,
+		0.5: Color.GREEN,
+		0.75: Color.VIOLET,
+		1.0: Color.BLUE,
+	}
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	var gradient := Gradient.new()
+	gradient.offsets = gradient_data.keys()
+	gradient.colors = gradient_data.values()
+
+	var gradient_texture := GradientTexture1D.new()
+	gradient_texture.gradient = gradient
+	gradient_texture.width = 3
+
+	n_sprite.texture = gradient_texture
+
+func _on_timer_timeout() -> void:
+	self.queue_free()
